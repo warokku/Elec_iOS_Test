ea3b2b686507df5be36ce7bf55ca921136da1092
Alex Mosolov <alexey.mosolov@Mac.lan>
2024-11-06 20:59:56 +0200

    Fix color extension

--- Elec_Test/Components/Color+HEX.swift
+++ Elec_Test/Components/Color+HEX.swift
@@ -8,28 +8,13 @@
 import SwiftUI
 
 extension Color {
-    init(hex: String) {
-        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int: UInt64 = 0
-        
-        let r, g, b, a: UInt64
-        switch hex.count {
-        case 3: // RGB (12-bit)
-            (r, g, b, a) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17, 255)
-        case 6: // RGB (24-bit)
-            (r, g, b, a) = (int >> 16, int >> 8 & 0xFF, int & 0xFF, 255)
-        case 8: // ARGB (32-bit)
-            (r, g, b, a) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-        default:
-            (r, g, b, a) = (1, 1, 1, 1) // Default to white for invalid input
-        }
-        
+    init(hex: UInt) {
         self.init(
             .sRGB,
-            red: Double(r) / 255,
-            green: Double(g) / 255,
-            blue: Double(b) / 255,
-            opacity: Double(a) / 255
+            red: Double((hex >> 24) & 0xff) / 255,
+            green: Double((hex >> 16) & 0xff) / 255,
+            blue: Double((hex >> 08) & 0xff) / 255,
+            opacity: Double((hex >> 00) & 0xff) / 255
         )
     }
 }
