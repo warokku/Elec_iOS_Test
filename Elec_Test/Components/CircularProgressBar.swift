ff7885cc17dc73bc6bd2ee066c43313c5516a8ad
Alex Mosolov <alexey.mosolov@Mac.lan>
2024-11-07 08:15:54 +0200

    Remove comments

--- Elec_Test/Components/CircularProgressBar.swift
+++ Elec_Test/Components/CircularProgressBar.swift
@@ -27,15 +27,6 @@ struct CircularProgressBar: View {
 
     var body: some View {
         ZStack {
-//            Circle()
-//                .stroke(lineWidth: 8.0)
-//                .foregroundColor(Color(hex: 0xFF323333))
-//            Circle()
-//                .trim(from: 0.0, to: CGFloat(min(progress, 1.0)))
-//                .stroke(style: StrokeStyle(lineWidth: 8.0, lineCap: .round, lineJoin: .round))
-//                .foregroundColor(Color(hex: 0xFFe1ad01))
-//                .rotationEffect(Angle(degrees: 270.0))
-//                .animation(.linear)
             CircularTimer(interval: 60, progress: 0.5)
 
             Text(text)
